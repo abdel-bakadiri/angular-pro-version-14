b99a8dd1dffa4eb547df005e07421da4d25ee0a4
Abdel <abdelouahab.bakadiri>
2023-03-12 00:24:19 +0100

    view encapsulation: Emulated, shadowDom and none

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,20 @@ import { AppComponent } from './app.component';
 import { LogInAndSignUpComponent } from './log-in-and-sign-up/log-in-and-sign-up.component';
 import { RememberMeComponent } from './remember-me/remember-me.component';
 import { MessageComponent } from './message/message.component';
+import { ExampleOneComponent } from './one/one.component';
+import { ExampleTwoComponent } from './two/two.component';
+import { ExampleThreeComponent } from './three/three.component';
 
 @NgModule({
-  declarations: [AppComponent, LogInAndSignUpComponent, RememberMeComponent, MessageComponent],
+  declarations: [
+    AppComponent,
+    LogInAndSignUpComponent,
+    RememberMeComponent,
+    MessageComponent,
+    ExampleOneComponent,
+    ExampleTwoComponent,
+    ExampleThreeComponent,
+  ],
   imports: [FormsModule, BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
