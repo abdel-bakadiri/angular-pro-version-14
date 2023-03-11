6e8ed80abe41d718bfa1ad760b0d37f41714ebf9
Abdel <abdelouahab.bakadiri>
2023-03-11 17:04:02 +0100

    inject a ng-template in ng-contianer using [ngTemplateOutlet]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,25 +17,6 @@ import { LogInAndSignUpComponent } from './log-in-and-sign-up/log-in-and-sign-up
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit, AfterContentInit {
-  @ViewChild('placeholder', { read: ViewContainerRef })
-  container!: ViewContainerRef;
-  @ViewChild('location')
-  location!: TemplateRef<null>;
-  title = 'Angular Pro';
-  loginComponent!: ComponentRef<LogInAndSignUpComponent>;
-
-  constructor() {}
-
-  ngAfterContentInit(): void {}
-
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.container.createEmbeddedView(this.location, {
-        $implicit: 'abdel',
-        location: 'Paris',
-      });
-    });
-  }
-  ngOnInit(): void {}
+export class AppComponent {
+  title = 'Angular Pro course';
 }
